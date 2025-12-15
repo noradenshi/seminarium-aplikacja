c3e6b46e0b7a758428ca485b7ba04747fa7937b2
noradenshi <74954748+noradenshi@users.noreply.github.com>
2025-12-15 02:09:31 +0100

    Added apk3

--- apk2/index.php
+++ apk2/index.php
@@ -5,27 +5,54 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8">
     <title>IRZ</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <style>
-        .center {text-align:center}
+        .center {
+            text-align: center
+        }
 
-        div {width:410px; height:209px}
+        div {
+            width: 410px;
+            height: 209px
+        }
 
         div * {
-        display:block; width:100%; margin:0; padding:0;
-        box-sizing:border-box; font-family:Arial; font-size:10pt
+            display: block;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Arial;
+            font-size: 10pt
         }
 
         p {
-        height:30px; padding:0.8ex 0; text-align:center; background-color:#f3f3ee;
-        border-style:solid; border-width:1px; border-color:gray; font-weight:bold
+            height: 30px;
+            padding: 0.8ex 0;
+            text-align: center;
+            background-color: #f3f3ee;
+            border-style: solid;
+            border-width: 1px;
+            border-color: gray;
+            font-weight: bold
         }
 
         textarea {
-        height:145px; resize:none; background-color:white; color:black;
-        border-style:solid; border-width:1px; border-top-width:0; border-color:gray;
+            height: 145px;
+            resize: none;
+            background-color: white;
+            color: black;
+            border-style: solid;
+            border-width: 1px;
+            border-top-width: 0;
+            border-color: gray;
         }
 
-        button {height:32px; margin-top:2px; font-weight:bold}
+        button {
+            height: 32px;
+            margin-top: 2px;
+            font-weight: bold
+        }
     </style>
 </head>
 
@@ -39,58 +66,50 @@
     </div>
 
     <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const recordsArea = document.getElementById('records-area');
-            const headLabel = document.getElementById('head');
-            const drawBtn = document.getElementById('draw-btn');
+        $(document).ready(function() {
+            const recordsArea = $('#records-area');
+            const headLabel = $('#head');
+            const drawBtn = $('#draw-btn');
 
             function updateView(data) {
                 if (!data) return;
 
-                headLabel.innerHTML = `Excel: ${data.maxExcel} &nbsp;Java: ${data.maxJava}`;
+                headLabel.html(`Excel: ${data.maxExcel} &nbsp;Java: ${data.maxJava}`);
 
                 if (data.records && data.records.length > 0) {
-                    recordsArea.value = data.records.join('\n');
-                    recordsArea.scrollTop = recordsArea.scrollHeight;
+                    recordsArea.val(data.records.join('\n'));
+                    recordsArea.scrollTop(recordsArea[0].scrollHeight);
                 } else {
-                    recordsArea.value = '';
-                }
-
-                
-            }
-
-            async function communicateWithServer(action = null) {
-                try {
-                    const options = {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    };
-
-                    if (action) {
-                        options.body = JSON.stringify({ action: action });
-                    }
-
-                    const response = await fetch('backend.php', options);
-
-                    if (!response.ok) {
-                        throw new Error(`Błąd serwera: ${response.status}`);
-                    }
-
-                    const data = await response.json();
-                    updateView(data);
-
-                } catch (error) {
-                    console.error('Szczegóły błędu:', error);
-                    headLabel.innerText = "Błąd połączenia. Sprawdź konsolę (F12).";
-                    headLabel.style.color = "red";
+                    recordsArea.val('');
                 }
             }
 
+            function communicateWithServer(action = null) {
+                $.ajax({
+                    url: 'backend.php',
+                    type: 'POST',
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    data: action ? JSON.stringify({
+                        action: action
+                    }) : null,
+
+                    success: function(data) {
+                        updateView(data);
+                    },
+
+                    error: function(xhr, status, error) {
+                        console.error('Szczegóły błędu:', status, error);
+                        headLabel.text("Błąd połączenia. Sprawdź konsolę (F12).");
+                        headLabel.css("color", "red");
+                    }
+                });
+            }
+
+            // reset przy starcie
             communicateWithServer('reset');
 
-            drawBtn.addEventListener('click', (e) => {
+            drawBtn.on('click', function(e) {
                 e.preventDefault();
                 communicateWithServer('draw');
             });
